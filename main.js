7b165a2350952eb554770cae779b139fa5e6a522
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-29 20:34:18 +0200

    clearRect function added to clear previous circle

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
-c.beginPath();
-c.arc(300, 300, 30, 0, Math.PI*2, false);
-c.strokeStyle = 'blue';
-c.stroke();
+
 let x = 200;
 function animate(){
     requestAnimationFrame(animate);
     console.log('How long it loops');
+    c.clearRect(0, 0, innerWidth, innerHeight);
     c.beginPath();
     c.arc(x, 200, 30, 0, Math.PI*2, false);
     c.strokeStyle = 'blue';
