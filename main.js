438ee9d41eb572b66a43f2d4eadbe4054bd73d98
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-29 20:57:22 +0200

    condition added to limit the move

--- main.js
+++ main.js
@@ -4,15 +4,26 @@ canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
 
 let x = 200;
+let y = 200;
+let dx = 4;
+let dy = 4;
+let radius = 30;
 function animate(){
     requestAnimationFrame(animate);
     console.log('How long it loops');
     c.clearRect(0, 0, innerWidth, innerHeight);
     c.beginPath();
-    c.arc(x, 200, 30, 0, Math.PI*2, false);
+    c.arc(x, y, radius, 0, Math.PI*2, false);
     c.strokeStyle = 'blue';
     c.stroke();
-    x +=1;
+    if(x + radius > innerWidth || x-radius < 0 ){
+        dx = -dx;
+    }
+    if(y + radius > innerHeight || y-radius < 0 ){
+        dy = -dy;
+    }
+    x +=dx;
+    y +=dy;
 
 }
 animate();
